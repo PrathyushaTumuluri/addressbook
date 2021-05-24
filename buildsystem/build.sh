6129b714676d7ea6c3b30c2e5ec4d30aef32197b
Prathyusha Tumuluri <ptumuluri@aligntech.com>
2021-05-24 18:36:44 +0530

    minor-change

--- buildsystem/build.sh
+++ buildsystem/build.sh
@@ -7,7 +7,7 @@ echo "build docker image"
 docker build -t addressbooknew:20210510 .
 
 echo "tag docker image"
-docker tag addressbook:20210510 pavaniprathyu/addressbooknew:20210510
+docker tag addressbooknew:20210510 pavaniprathyu/addressbooknew:20210510
 
 echo "push docker image"
 docker push pavaniprathyu/addressbooknew:20210510
