0f4bdf343e9441d1513f4447446e40b1e87f8ba5
Prathyusha Tumuluri <ptumuluri@aligntech.com>
2021-05-24 18:58:55 +0530

    change-port

--- buildsystem/deploy.sh
+++ buildsystem/deploy.sh
@@ -5,8 +5,8 @@ docker_web_pull_result=$(docker pull pavaniprathyu/addressbooknew:20210510 2>&1)
 echo $docker_web_pull_result
 
 echo "run docker image"
-docker run -d -p 6666:8080 pavaniprathyu/addressbooknew:20210510
+docker run -d -p 8070:8080 pavaniprathyu/addressbooknew:20210510
 
 echo "test curl"
-curl_result=$(curl -v http://localhost:6666/addressbook/ping 2>&1)
+curl_result=$(curl -v http://localhost:8070/addressbook/ping 2>&1)
 echo $curl_result
